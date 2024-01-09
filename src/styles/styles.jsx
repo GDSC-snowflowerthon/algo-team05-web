b95b53a3b853f4f7bdbd7122ed3a83cfc2c4b0a4
bianbbc87 <bian87@dgu.ac.kr>
2024-01-09 22:01:21 +0900

    [ADD] #2 - main 기본 Wrapper style 추가

--- src/styles/styles.jsx
+++ src/styles/styles.jsx
@@ -6,3 +6,19 @@ import { theme } from "@/styles/theme";
 export const Sample = styled.div`
   background-color: ${theme.yellow};
 `;
+
+/* 기본 Wrapper 스타일 */
+export const Wrapper = styled.div`
+  position: relative;
+  margin: 0 auto;
+  width: 100%;
+  height: 100%;
+  min-height: 99vh;
+  display: flex;
+  flex-direction: column;
+  max-width: 390px;
+  background-color: #f9faff;
+  align-items: center;
+`;
+
+/* other pages... */
