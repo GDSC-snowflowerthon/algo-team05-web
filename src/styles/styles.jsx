42b5cc458dfec4d1973bb13c9a8e94a1a4fc337e
bianbbc87 <bian87@dgu.ac.kr>
2024-01-09 20:33:31 +0900

    [CHORE] #1 - main 라이브러리, 디렉토리 세팅 완료

---
+++ src/styles/styles.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { theme } from "@/styles/theme";
+
+// 페이지별로 용도 나눠주세요
+/**  샘플 페이지 스타일 **/
+export const Sample = styled.div`
+  background-color: ${theme.yellow};
+`;
---
+++ src/styles/theme.jsx
@@ -0,0 +1,18 @@
+export const theme = {
+  // 색상 이름: "색상 코드" <= 형태로 정의해주세요.
+  // 사용은 theme import 후 theme.red와 같이 사용
+  /* 아래 색상은 예시이기 때문에, 본인이 사용하는 컬러가 있다면 따로 정의해주세요.
+      재훈님 디자인 완료 후 일괄 정리 예정*/
+
+  red: "#D22C26",
+  orange: "#FFA751",
+  yellow: "#FFE03D",
+  black: "#000000",
+  green: "#34BA05",
+  white: "#FFFFFF",
+  gray: "#CFCFCF",
+  blue: "#00A3FF",
+};
+
+// 전역 색상 지정(다크 모드, 화이트 모드 등 속성 설정용)
+export const mode = {};
