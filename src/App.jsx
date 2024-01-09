4b4336de9f7dcfb6be3dd377a7eede6b50fddf7f
bianbbc87 <bian87@dgu.ac.kr>
2024-01-10 00:32:59 +0900

    [ADD] #2 - click-content 네비게이션 바 css 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import TestPage from "@/pages/test/TestPage";
 import ClickContent from "@/pages/click-content/ClickContent";
+import Navigation from "@/components/Navigation";
 
 function App() {
   return (
     <>
+      <Navigation />
       <Routes>
         <Route path="/test" element={<TestPage />} />
         <Route path="/message-content" element={<ClickContent />} />
