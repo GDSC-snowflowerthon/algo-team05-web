8769a0cc590124679f209bea3d19626fc5646ec9
bianbbc87 <bian87@dgu.ac.kr>
2024-01-10 19:05:54 +0900

    [ADD] #4 - map 지역 클릭 시 색상 변경용 컴포넌트 추가

--- src/components/map/SelectedMap.jsx
+++ src/components/map/SelectedMap.jsx
@@ -1,5 +1,16 @@
 import styled from "styled-components";
 
+export const BeforeMap = styled.div`
+  position: relative;
+  top: 20px;
+  width: 390px;
+  height: 552px;
+`;
+
 export default function SelectedMap() {
-  return <div></div>;
+  return (
+    <>
+      <BeforeMap></BeforeMap>
+    </>
+  );
 }
