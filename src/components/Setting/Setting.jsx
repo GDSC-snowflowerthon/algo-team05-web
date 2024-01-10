42b6a554c8a408ce1c7d475d49d34c21b8fb04b6
bianbbc87 <bian87@dgu.ac.kr>
2024-01-11 03:15:50 +0900

    [ADD] setting dummy와 options css 추가

--- src/components/Setting/Setting.jsx
+++ src/components/Setting/Setting.jsx
@@ -1,6 +1,64 @@
 import styled from "styled-components";
 import { useState } from "react";
 import PropTypes from "prop-types";
+import SelectBar from "@/components/Setting/SelectBar";
+import { codes } from "@/data/CountryCode";
+import { area } from "@/data/Area";
+
+export default function Setting({ setIsShow }) {
+  const [isOn, setIsOn] = useState(true);
+
+  const handleOn = () => {
+    setIsOn(!isOn);
+  };
+
+  const handleSave = () => {
+    setIsShow(false);
+  };
+
+  return (
+    <Wrapper>
+      <SettingBox>
+        <Title>설정</Title>
+        <ContentBox>
+          <Content top="19px">언어(lenguage)</Content>
+          <SelectBar data={codes} />
+          <Content top="11px">지역(area)</Content>
+          <SelectBar data={area} />
+          <Content top="11px">번역(translate)</Content>
+          {isOn ? (
+            <OnOffBox onClick={handleOn}>
+              <OnBtn>On</OnBtn>
+              <OffBtn>Off</OffBtn>
+            </OnOffBox>
+          ) : (
+            <OnOffBox onClick={handleOn}>
+              <OnBtn bg="transparent" shadow="none" color="#f9faff">
+                On
+              </OnBtn>
+              <OffBtn
+                bg="#F9FAFF"
+                shadow="0px 0px 7px 0px rgba(0, 0, 0, 0.25)"
+                color="#333d42"
+              >
+                Off
+              </OffBtn>
+            </OnOffBox>
+          )}
+        </ContentBox>
+        <ButtonStyle top="27px" onClick={handleSave}>
+          저장
+        </ButtonStyle>
+      </SettingBox>
+    </Wrapper>
+  );
+}
+
+/* <Setting> 컴포넌트의 propTypes를 정의하면 useState 함수도 props로 사용할 수 있다.
+ isRequired : 필수 prop임을 의미한다. */
+Setting.propTypes = {
+  setIsShow: PropTypes.func.isRequired,
+};
 
 export const Wrapper = styled.div`
   position: fixed;
@@ -111,56 +169,3 @@ export const ButtonStyle = styled.button`
     -4px -4px 6px 0px rgba(255, 255, 255, 0.5);
   cursor: pointer;
 `;
-
-export default function Setting({ setIsShow }) {
-  const [isOn, setIsOn] = useState(true);
-
-  const handleOn = () => {
-    setIsOn(!isOn);
-  };
-
-  const handleSave = () => {
-    setIsShow(false);
-  };
-
-  return (
-    <Wrapper>
-      <SettingBox>
-        <Title>설정</Title>
-        <ContentBox>
-          <Content top="19px">언어(lenguage)</Content>
-          <Content top="11px">지역(area)</Content>
-          <Content top="11px">번역(translate)</Content>
-          {isOn ? (
-            <OnOffBox onClick={handleOn}>
-              <OnBtn>On</OnBtn>
-              <OffBtn>Off</OffBtn>
-            </OnOffBox>
-          ) : (
-            <OnOffBox onClick={handleOn}>
-              <OnBtn bg="transparent" shadow="none" color="#f9faff">
-                On
-              </OnBtn>
-              <OffBtn
-                bg="#F9FAFF"
-                shadow="0px 0px 7px 0px rgba(0, 0, 0, 0.25)"
-                color="#333d42"
-              >
-                Off
-              </OffBtn>
-            </OnOffBox>
-          )}
-        </ContentBox>
-        <ButtonStyle top="27px" onClick={handleSave}>
-          저장
-        </ButtonStyle>
-      </SettingBox>
-    </Wrapper>
-  );
-}
-
-/* <Setting> 컴포넌트의 propTypes를 정의하면 useState 함수도 props로 사용할 수 있다.
- isRequired : 필수 prop임을 의미한다. */
-Setting.propTypes = {
-  setIsShow: PropTypes.func.isRequired,
-};
