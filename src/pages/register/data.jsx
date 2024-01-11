0a72296d27f57489df2646001c5e3875b8ae7960
solmee <87332713+ssolfa@users.noreply.github.com>
2024-01-12 00:11:11 +0900

    [ADD] data.jsx 추가

---
+++ src/pages/register/data.jsx
@@ -0,0 +1,228 @@
+
+export const languageOptions = [
+    { value: 'ko', label: '한국어' },
+  { value: 'en', label: 'English' },
+  { value: 'ja', label: '日本語' },
+  { value: 'zh-CN', label: '简体中文' },
+  { value: 'zh-TW', label: '繁體中文' },
+  { value: 'vi', label: 'Tiếng Việt' },
+  { value: 'id', label: 'Bahasa Indonesia' },
+  { value: 'th', label: 'ภาษาไทย' },
+  { value: 'de', label: 'Deutsch' },
+  { value: 'ru', label: 'Русский' },
+  { value: 'es', label: 'Español' },
+  { value: 'it', label: 'Italiano' },
+  { value: 'fr', label: 'Français' },
+  ];
+
+export const regionOptions = [
+    { value: '강원도', label: '강원도' },
+    { value: '경기도', label: '경기도' },
+    { value: '경상남도', label: '경상남도' },
+    { value: '경상북도', label: '경상북도' },
+    { value: '광주광역시', label: '광주광역시' },
+    { value: '대구광역시', label: '대구광역시' },
+    { value: '대전광역시', label: '대전광역시' },
+    { value: '부산광역시', label: '부산광역시' },
+    { value: '서울특별시', label: '서울특별시' },
+    { value: '세종시', label: '세종시' },
+    { value: '인천광역시', label: '인천광역시' },
+    { value: '울산광역시', label: '울산광역시' },
+    { value: '전라북도', label: '전라북도' },
+    { value: '전라남도', label: '전라남도' },
+    { value: '제주도', label: '제주도' },
+    { value: '충청북도', label: '충청북도' },
+    { value: '충청남도', label: '충청남도' },
+  ];
+
+export const seoulDistricts = [
+    {value:"0", label: "강남구"}, 
+    {value:"1", label: "강동구"}, 
+    {value:"2", label: "강북구"}, 
+    {value:"3", label: "강서구"}, 
+    {value:"4", label: "관악구"}, 
+    {value:"5", label: "광진구"}, 
+    {value:"6", label: "구로구"}, 
+    {value:"7", label: "금천구"}, 
+    {value:"8", label: "노원구"}, 
+    {value:"9", label: "도봉구"}, 
+    {value:"10", label: "마포구"}, 
+    {value:"11", label: "서대문구"}, 
+    {value:"12", label: "서초구"}, 
+    {value:"13", label: "성동구"}, 
+    {value:"14", label: "성북구"}, 
+    {value:"15", label: "송파구"}, 
+    {value:"16", label: "양천구"}, 
+    {value:"17", label: "영등포구"}, 
+    {value:"18", label: "은평구"}, 
+    {value:"19", label: "종로구"}, 
+    {value:"20", label: "중구"}, 
+    {value:"21", label: "중량구"}, 
+  ];
+  
+export const gyeonggiDistricts = [
+      { value: "22", label: "수원시" },
+      { value: "23", label: "성남시" },
+      { value: "24", label: "안양시" },
+      { value: "25", label: "부천시" },
+      { value: "26", label: "광명시" },
+      { value: "27", label: "동두천시" },
+      { value: "28", label: "평택시" },
+      { value: "29", label: "안산시" },
+      { value: "30", label: "고양시" },
+      { value: "31", label: "과천시" },
+      { value: "32", label: "구리시" },
+      { value: "33", label: "남양주시" },
+      { value: "34", label: "오산시" },
+      { value: "35", label: "시흥시" },
+      { value: "36", label: "군포시" },
+      { value: "37", label: "의왕시" },
+      { value: "38", label: "하남시" },
+      { value: "39", label: "용인시" },
+      { value: "40", label: "파주시" },
+      { value: "41", label: "이천시" },
+      { value: "42", label: "안성시" },
+      { value: "43", label: "김포시" },
+      { value: "44", label: "화성시" },
+      { value: "45", label: "광주시" },
+      { value: "46", label: "양주시" },
+      { value: "47", label: "포천시" },
+      { value: "48", label: "여주시" },
+    ];
+  
+export const ulsanDistricts = [
+    { value: "49", label: "중구" },
+    { value: "50", label: "남구" },
+    { value: "51", label: "동구" },
+    { value: "52", label: "북구" },
+    ];
+  
+export const daejeonDistricts = [
+    { value: "53", label: "동구" },
+    { value: "54", label: "중구" },
+    { value: "55", label: "서구" },
+    { value: "56", label: "유성구" },
+    { value: "57", label: "대덕구" },
+    ];
+  
+export const gwangjuDistricts = [
+    { value: "58", label: "동구" },
+    { value: "59", label: "서구" },
+    { value: "60", label: "남구" },
+    { value: "61", label: "북구" },
+    { value: "62", label: "광산구" },
+    ];
+  
+export const icDistricts = [
+    { value: "63", label: "중구" },
+    { value: "64", label: "동구" },
+    { value: "65", label: "미추홀구" },
+    { value: "66", label: "연수구" },
+    { value: "67", label: "남동구" },
+    { value: "68", label: "부평구" },
+    { value: "69", label: "계양구" },
+    { value: "70", label: "서구" },
+  
+    ];
+export const dagueDistricts = [
+    { value: "71", label: "중구" },
+    { value: "72", label: "동구" },
+    { value: "73", label: "서구" },
+    { value: "74", label: "남구" },
+    { value: "75", label: "북구" },
+    { value: "76", label: "수성구" },
+    { value: "77", label: "달서구" },
+    ];
+  
+export const busanDistricts = [
+    { value: "78", label: "중구" },
+    { value: "79", label: "서구" },
+    { value: "80", label: "동구" },
+    { value: "81", label: "영도구" },
+    { value: "82", label: "부산진구" },
+    { value: "83", label: "동래구" },
+    { value: "84", label: "남구" },
+    { value: "85", label: "북구" },
+    { value: "86", label: "강서구" },
+    { value: "87", label: "해운대구" },
+    { value: "88", label: "사하구" },
+    { value: "89", label: "금정구" },
+    { value: "90", label: "연제구" },
+    { value: "91", label: "수영구" },
+    { value: "92", label: "사상구" },
+    ];
+export const jejuDistricts = [
+    { value: "93", label: "제주도시" },
+    { value: "94", label: "서귀포시" },
+    ];
+  
+export const gangwonDistricts = [
+    { value: "95", label: "춘천시" },
+    { value: "96", label: "원주시" },
+    { value: "97", label: "강릉시" },
+    { value: "98", label: "동해시" },
+    { value: "99", label: "태백시" },
+    { value: "100", label: "속초시" },
+    { value: "101", label: "삼척시" },
+    ];
+  
+export const cbDistricts = [
+    { value: "102", label: "청주시" },
+    { value: "103", label: "충주시" },
+    { value: "104", label: "제천시" },
+    ];
+  
+export const cnDistricts = [
+    { value: "105", label: "천안시" },
+    { value: "106", label: "공주시" },
+    { value: "107", label: "보령시" },
+    { value: "108", label: "아산시" },
+    { value: "109", label: "서산시" },
+    { value: "110", label: "논산시" },
+    { value: "111", label: "계룡시" },
+    { value: "112", label: "당진시" },
+    ];
+  
+export const jbDistricts = [
+    { value: "113", label: "전주시" },
+    { value: "114", label: "군산시" },
+    { value: "115", label: "익산시" },
+    { value: "116", label: "정읍시" },
+    { value: "117", label: "남원시" },
+    { value: "118", label: "김제시" },
+    ];
+  
+ export const jnDistricts = [
+    { value: "119", label: "목포시" },
+    { value: "120", label: "여수시" },
+    { value: "121", label: "순천시" },
+    { value: "122", label: "나주시" },
+    { value: "123", label: "광양시" },
+    ];
+  
+export const kbDistricts = [
+    { value: "332", label: "포항시" },
+    { value: "333", label: "경주시" },
+    { value: "422", label: "김천시" },
+    { value: "434", label: "안동시" },
+    { value: "404", label: "구미시" },
+    { value: "200", label: "영주시" },
+    { value: "201", label: "영천시" },
+    { value: "202", label: "상주시" },
+    { value: "203", label: "문경시" },
+    { value: "204", label: "경산시" },
+    ];
+  
+export const knDistricts = [
+    { value: "124", label: "창원시" },
+    { value: "125", label: "진주시" },
+    { value: "126", label: "통영시" },
+    { value: "127", label: "사천시" },
+    { value: "128", label: "김해시" },
+    { value: "129", label: "밀양시" },
+    { value: "130", label: "거제시" },
+    { value: "131", label: "양산시" },
+    ];
+
+export const sejongDistricts = [
+    ];
