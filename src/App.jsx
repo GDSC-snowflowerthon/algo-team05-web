bb42944688fa0f4d0b52bc2c09d2df67d2e8e5e2
bianbbc87 <bian87@dgu.ac.kr>
2024-01-11 16:11:22 +0900

    [ADD] app에 웹 푸시 함수 작성(삭제 예정)

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,35 @@ import SelectMap from "@/pages/map/SelectMap";
 import Shelter from "@/pages/shelter/Shelter";
 import { requestPermission } from "./firebase-massaging.js";
 
+const useNotification = (title, options) => {
+  if (!("Notification" in window)) {
+    return;
+  }
+
+  const fireNotif = () => {
+    /* 권한 요청 부분 */
+    if (Notification.permission !== "granted") {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          /* 권한을 요청받고 nofi를 생성해주는 부분 */
+          new Notification(title, options);
+        } else {
+          return;
+        }
+      });
+    } else {
+      /* 권한이 있을때 바로 noti 생성해주는 부분 */
+      new Notification(title, options);
+    }
+  };
+  return fireNotif;
+};
+
 function App() {
+  const triggerNotif = useNotification("Test Noti", {
+    body: "notification body test",
+  });
+
   useEffect(() => {
     // 컴포넌트가 마운트될 때 한 번만 호출
     requestPermission();
@@ -21,7 +49,7 @@ function App() {
         <Route path="/map" element={<SelectMap />} />
         <Route path="/shelter-list" element={<Shelter />} />
       </Routes>
-      <button>클릭</button>
+      <button onClick={triggerNotif}>클릭</button>
     </>
   );
 }
